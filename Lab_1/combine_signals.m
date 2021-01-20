027a65fa0215ee0720beccb7b438e52416d44206
SamSSLF <samantha.foong19@imperial.ac.uk>
2021-01-20 18:32:41 +0000

    Ex 3-5 completed

---
+++ Lab_1/combine_signals.m
@@ -0,0 +1,16 @@
+function signal = combine_signals(s1,s2);
+% function taes two periodic signals s1 and s2, 
+% adds them together into a composite signal, 
+% then plots it
+    signal = s1 + s2;
+    plot(s1(1:100),"LineStyle","-.");
+    hold on;
+    plot(s2(1:100),"LineStyle","-.");
+    hold on;
+    plot(signal(1:100),"LineWidth",1.0,"Color",[0,0,0]);
+    hold off;
+    set(gca,'XGrid','off','YGrid','on');
+    title('Combined Signal');
+    xlabel('Sample');
+    ylabel('Amplitude')
+end
--- Lab_1/sine_gen.m
+++ Lab_1/sine_gen.m
@@ -1,4 +1,4 @@
-function [sig] = sine_gen(amp, f, fs, T); 
+function sig = sine_gen(amp, f, fs, T); 
     dt = 1/fs;
     t  = 0:dt:T;
     sig = amp*sin(2*pi*f*t);
